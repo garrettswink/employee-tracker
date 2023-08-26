b19b269fef3f55afa43094c1157f11083bef01f1
Garrett <garrettswink421@gmail.com>
2023-08-26 00:39:44 -0400

    schema

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY.
+    department_name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    department_id INT NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (department_id) 
+    REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+);
